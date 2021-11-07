cf799de305a88be71b3a92dd07aa459fe2e8c147
toby1122 <78290795+hyunlee1122@users.noreply.github.com>
2021-11-07 15:56:25 -0500

    SVA hello-world

---
+++ wee8(HW)/README.md
@@ -0,0 +1,3 @@
+# Hello-world-2023 Week 8 Homework
+
+This is a plant shown in AR.
---
+++ wee8(HW)/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import BaseHTTPServer
+import SimpleHTTPServer
+import ssl
+
+# Variables you can modify
+# https://192.168.250.72:4509
+bind_to_address = '172.168.1.12'
+server_port = 4509
+ssl_key_file = "server1.example.com.key"
+ssl_certificate_file = "server1.example.com.pem"
+
+
+# Don't modify anything below
+
+httpd = BaseHTTPServer.HTTPServer(
+    (bind_to_address, server_port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True,
+                               keyfile=ssl_key_file,
+                               certfile=ssl_certificate_file)
+httpd.serve_forever()
